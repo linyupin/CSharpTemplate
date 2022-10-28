c2d5eda531949a84985ac43a64591635220a8d8d
Lin Yu-Pin <baby520370tw@gmail.com>
2022-10-28 09:18:27 +0800

    create base struct

---
+++ project/MainProject/MainProject/View/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MainProject.ViewModel;
+
+namespace MainProject.View {
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window {
+        MainWindowVM _MainVM;
+        public MainWindow() {
+            InitializeComponent();
+            _MainVM = new MainWindowVM();
+            DataContext = _MainVM;
+        }
+    }
+}
---
+++ project/MainProject/MainProject/ViewModel/MainWindowVM.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainProject.ViewModel {
+    public class MainWindowVM {
+        public MainWindowVM() {
+
+        }
+
+        // **** Init Function ****
+        #region
+        #endregion
+
+        // **** Private Parameter ****
+        #region
+        #endregion
+
+        // **** Public Parameter ****
+        #region
+        #endregion
+
+        // **** Binding Parameter ****
+        #region
+        #endregion
+
+        // **** Binding Command ****
+        #region
+        #endregion
+
+        // **** Private Function ****
+        #region
+        #endregion
+
+        // **** Public Function ****
+        #region
+        #endregion
+
+
+    }
+}
